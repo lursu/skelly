02f78deb117a52ab6a41e76425367567c08cfa91
lursu <lursu@barracuda.com>
2016-01-30 11:41:21 -0800

    fixed some errors

--- builder/file.go
+++ builder/file.go
@@ -16,12 +16,12 @@ type File struct {
 
 func (f File) Write() error {
 	buffer := bytes.NewBufferString(f.path)
-	fullName := buffer.WriteString(f.name)
-	return ioutil.WriteFile(fullName.String(), contents, permission)
+	buffer.WriteString(f.name)
+	return ioutil.WriteFile(buffer.String(), f.contents, f.permission)
 }
 
 func BuildFile(key string, name string, path string, permission os.FileMode) *File {
-	return File{
+	return &File{
 		permission: permission,
 		name:       name,
 		path:       path,
@@ -31,38 +31,37 @@ func BuildFile(key string, name string, path string, permission os.FileMode) *Fi
 
 func getFileContents(key string) []byte {
 	result := new(bytes.Buffer)
-	funcPath := bytes.NewBufferString(key)
 	viper := viper.New()
 	fileMap := viper.GetStringMap(key)
 	for k, v := range fileMap {
 		switch k {
 		case "package":
 			result.WriteString("package")
-			result.WriteString(v)
+			result.WriteString(v.(string))
 		case "imports":
 			result.WriteString("import (\n")
-			result.WriteString(v)
+			result.WriteString(v.(string))
 		default:
-			buildFuncContents(key, result)
+			getFuncContents(key, result)
 		}
 	}
 	return result.Bytes()
 }
 
-func getFuncContents(baseKey string, b *Buffer) {
+func getFuncContents(baseKey string, b *bytes.Buffer) {
 	viper := viper.New()
 	funcPath := bytes.NewBufferString(baseKey)
 	funcPath.WriteString(".functions")
 	funcsMap := viper.GetStringMap(funcPath.String())
-	for k, v := range funcsMap {
+	for _, v := range funcsMap {
 		b.WriteString("\n")
-		buildFunc(v.(map[string]string), &b)
+		buildFunc(v.(map[string]string), b)
 	}
 }
 
-func buildFunc(funcMap map[string]string, b *Buffer) {
+func buildFunc(funcMap map[string]string, b *bytes.Buffer) {
 	b.WriteString("func ")
-	for k, v := range funcMap {
+	for _, v := range funcMap {
 		b.WriteString(v)
 	}
 	b.WriteString("}\n")
